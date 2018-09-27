6f8704603e8f99bb53b386da88ec7026ea15da45
Xiangshun Bei <bxs3514@hotmail.com>
2018-09-27 02:25:44 -0700

    Compute shader: Initialize compute shader
    Added compute shader compiler, test compute shader.

--- OpenGLEngine/src/Render/ForwardPlusRender.cpp
+++ OpenGLEngine/src/Render/ForwardPlusRender.cpp
@@ -1,7 +1,13 @@
 #include "ForwardPlusRender.h"
 
+using Math::Vector4;
+
 ForwardPlusRender::ForwardPlusRender()
 {
+    m_frustums[0] = { 0.0f, 0.0f, 0.0f, 0.0f };
+    m_frustums[1] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    m_frustums[2] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    m_frustums[3] = { 1.0f, 1.0f, 1.0f, 1.0f };
 }
 
 ForwardPlusRender::~ForwardPlusRender()
@@ -13,16 +19,19 @@ BOOL ForwardPlusRender::initialize()
     BOOL result = TRUE;
 
     m_gridFrustumComputeShader.setComputeShader("GridFrustumsCompute.cs");
-    result = m_gridFrustumComputeShader.compileComputeShader();
+    result = m_gridFrustumComputeShader.compileComputeShader(); 
 
     assert(result == TRUE);
 
-    glGenBuffers(1, &m_frustumVerticesSsbo);
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_frustumVerticesSsbo);
+    m_gridFrustumComputeShader.useProgram();
 
+    glGenBuffers(1, &m_frustumVerticesSsbo); 
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_frustumVerticesSsbo); 
+    glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(m_frustums), static_cast<void*>(&(m_frustums[0])), GL_DYNAMIC_COPY); 
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, m_frustumVerticesSsbo); 
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);    
 
-
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+    m_gridFrustumComputeShader.FinishProgram();
 
     return result;
 }
@@ -35,7 +44,24 @@ void ForwardPlusRender::update()
 void ForwardPlusRender::draw()
 {
     m_gridFrustumComputeShader.useProgram();
+
     glDispatchCompute(1, 1, 1);
     glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+
     m_gridFrustumComputeShader.FinishProgram();
+
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_frustumVerticesSsbo);
+    Vector4* gpuFrustumsPtr =
+        static_cast<Vector4*>(glMapBuffer(GL_SHADER_STORAGE_BUFFER, GL_READ_ONLY));
+
+    /*for (int i = 0; i < 4; ++i)
+    {
+        gpuFrustumsPtr[i] = m_frustums[i];
+    }
+    glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
+
+    Vector4* gpuFrustumsPtr2 =
+        static_cast<Vector4*>(glMapBuffer(GL_SHADER_STORAGE_BUFFER, GL_MAP_WRITE_BIT | GL_MAP_READ_BIT));*/
+
+    glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
 }
