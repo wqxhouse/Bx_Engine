2dc3e77ef0ce640e2510635f92eec2fb8c70ae89
xbei <xbei@qti.qualcomm.com>
2018-10-22 13:28:11 -0700

    Forward+: Extend number of max lights to 2048

--- OpenGLTemplate/src/OpenGLTemplate.cpp
+++ OpenGLTemplate/src/OpenGLTemplate.cpp
@@ -138,7 +138,7 @@ void OpenGLTemplate::createDefaultScene()
         new Trans(glm::vec3( 0.0f, 0.0f, -3.0f), glm::vec3(), glm::vec3(0.0f, 1.0f, 0.0f)));
     pScene->addModel("../resources/models/sphere/sphere.obj", "../resources/models/sphere/sphere.mtl",
         new Trans(glm::vec3(-3.0f, 0.0f, -3.0f), glm::vec3(), glm::vec3(0.0f, 1.0f, 0.0f)));*/
-    
+
     /*pScene->addModel("../resources/models/sponza/sponza_big.obj", "../resources/models/sponza/sponza_big.mtl",
         new Trans(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(), glm::vec3(0.0f, 1.0f, 0.0f)));*/
 
@@ -152,7 +152,7 @@ void OpenGLTemplate::createDefaultScene()
     pScene->addModel(
         "../resources/models/dragon/dragon.obj",
         "../resources/models/dragon/dragon.mtl",
-        new Trans(glm::vec3(0.0f, -30.0f, 0.0f), glm::vec3(), glm::vec3(0.0f, 1.0f, 0.0f)));*/
+        new Trans(glm::vec3(0.0f, -30.0f, 0.0f), glm::vec3(), glm::vec3(0.0f, 1.0f, 0.0f)));
 
     /*pScene->addModel(
         "../resources/models/cornellbox/CornellBox-Sphere.obj",
