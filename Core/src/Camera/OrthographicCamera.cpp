05e75568078f93f7cb246aa5be4c84148c88f498
bxs3514 <xbei@qti.qualcomm.com>
2018-11-17 23:53:54 -0800

    Core: Camera - Wrapped camera classes with namespace
    - Wrapped camera classes with namespace

--- Core/src/Camera/OrthographicCamera.cpp
+++ Core/src/Camera/OrthographicCamera.cpp
@@ -1,29 +1,32 @@
 #include "Camera.h"
 #include "../Math/Math.h"
 
-OrthographicCamera::OrthographicCamera(
-    const glm::vec3&    pos,
-    const glm::vec3&    center,
-    const glm::vec3&    up,
-    const float         speed,
-    const BxsRectangle& viewport,
-    const float         nearClip,
-    const float         farClip)
-    : Camera(ORTHOGRAPHIC_CAM, pos, center, up, speed, nearClip, farClip),
-      m_viewport(viewport)
+namespace Camera
 {
-    m_projectionMatrix = Math::orthographicProjectionMatrix(viewport, nearClip, farClip);
-}
+    OrthographicCamera::OrthographicCamera(
+        const glm::vec3&    pos,
+        const glm::vec3&    center,
+        const glm::vec3&    up,
+        const float         speed,
+        const BxsRectangle& viewport,
+        const float         nearClip,
+        const float         farClip)
+        : CameraBase(ORTHOGRAPHIC_CAM, pos, center, up, speed, nearClip, farClip),
+        m_viewport(viewport)
+    {
+        m_projectionMatrix = Math::orthographicProjectionMatrix(viewport, nearClip, farClip);
+    }
 
-OrthographicCamera::~OrthographicCamera()
-{
-}
+    OrthographicCamera::~OrthographicCamera()
+    {
+    }
 
-void OrthographicCamera::update(float deltaTime)
-{
-    rotate(static_cast<float>(-callbackInfo.cursorPosCallBack.delta_y) * CAMERA_SENSATIVE,
-           static_cast<float>(callbackInfo.cursorPosCallBack.delta_x) * CAMERA_SENSATIVE);
+    void OrthographicCamera::update(float deltaTime)
+    {
+        rotate(static_cast<float>(-callbackInfo.cursorPosCallBack.delta_y) * CAMERA_SENSATIVE,
+            static_cast<float>(callbackInfo.cursorPosCallBack.delta_x) * CAMERA_SENSATIVE);
 
-    callbackInfo.cursorPosCallBack.reset();
-    Camera::update(deltaTime);
+        callbackInfo.cursorPosCallBack.reset();
+        CameraBase::update(deltaTime);
+    }
 }
