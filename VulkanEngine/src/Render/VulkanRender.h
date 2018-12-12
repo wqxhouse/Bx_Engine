6c782e5afbb5b1ef1243c2aede1fb8d476309423
xbei <xbei@qti.qualcomm.com>
2018-12-11 17:04:17 -0800

    Vulkan: Initialized Vulkan render interface
    - Initialized Vulkan render interface

--- VulkanEngine/src/Render/VulkanRender.h
+++ VulkanEngine/src/Render/VulkanRender.h
@@ -8,3 +8,47 @@
 //================================================================================================
 
 #pragma once
+
+#include <Scene/RenderScene.h>
+
+namespace VulkanEngine
+{
+	namespace Render
+	{
+		class VulkanRenderBase
+		{
+		public:
+			VulkanRenderBase(const Scene::RenderScene* pScene);
+			~VulkanRenderBase();
+
+			virtual void update(const float delta) = 0;
+			virtual void render() = 0;
+
+		protected:
+			const Scene::RenderScene* m_pScene;
+		};
+
+		class VulkanForwardRender : public VulkanRenderBase
+		{
+		public:
+			VulkanForwardRender(const Scene::RenderScene* pScene);
+			~VulkanForwardRender();
+
+			void update(const float delta);
+			void render();
+
+		private:
+
+		};
+
+		class VulkanDeferredRender : public VulkanRenderBase
+		{
+		public:
+			VulkanDeferredRender(const Scene::RenderScene* pScene);
+			~VulkanDeferredRender();
+
+			void update(const float delta);
+			void render();
+		};
+	}
+}
