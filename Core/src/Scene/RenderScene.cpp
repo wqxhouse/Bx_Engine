a1a511bf9b36e7b9be53ad8f01155c59411ae93a
xbei <xbei@qti.qualcomm.com>
2019-01-09 13:56:06 -0800

    Core: Scene - Added type list and GET functions
    - Added scene object type list
    - Added static function to get the max size of all scene objects
    - Added GET functions for all objects

--- Core/src/Scene/RenderScene.cpp
+++ Core/src/Scene/RenderScene.cpp
@@ -11,12 +11,26 @@
 
 namespace Scene
 {
+    const std::vector<const type_info*> RenderScene::m_typeList =
+    {
+        &typeid(Object::Camera::ProspectiveCamera),
+        &typeid(Object::Camera::OrthographicCamera),
+        &typeid(Object::Light::DirectionalLight),
+        &typeid(Object::Light::PointLight),
+        &typeid(Object::Light::SpotLight),
+        &typeid(Object::Model::ModelObject)
+    };
+
     RenderScene::RenderScene(
         const Setting* const     pSetting,
+        const UINT               maxObjNum,
         Memory::MemoryPoolArena* pObjArena)
         : m_pSetting(pSetting),
-          m_pObjArena(pObjArena)
+          m_pObjArena(pObjArena),
+          m_maxObjNum(maxObjNum),
+          m_objNum(0)
     {
+        //m_typeList.push_back(typeid(Object::Camera::ProspectiveCamera));
     }
 
     RenderScene::RenderScene(
@@ -27,6 +41,7 @@ namespace Scene
           m_pObjArena(pObjArena)
     {
         // TODO: Load scene from XML file
+        NotImplemented();
     }
 
     RenderScene::~RenderScene()
--- Core/src/Scene/RenderScene.h
+++ Core/src/Scene/RenderScene.h
@@ -23,6 +23,7 @@ namespace Scene
     public:
         RenderScene(
             const Setting* const     pSetting,
+            const UINT               maxObjNum,
             Memory::MemoryPoolArena* pObjArena);
         
         RenderScene(
@@ -35,6 +36,8 @@ namespace Scene
         INLINE void AddProspectiveCamera(
             const Object::Camera::ProspectiveCameraCreateInfo& prosCamCreateInfo)
         {
+            IncrementObjNum();
+
             m_pCameraList.push_back(
                 BX_NEW(Object::Camera::ProspectiveCamera, *m_pObjArena)(prosCamCreateInfo));
         }
@@ -42,6 +45,8 @@ namespace Scene
         INLINE void AddOrthographicCamera(
             const Object::Camera::OrthographicCameraCreateInfo& orthoCamCreateInfo)
         {
+            IncrementObjNum();
+
             m_pCameraList.push_back(
                 BX_NEW(Object::Camera::OrthographicCamera, *m_pObjArena)(orthoCamCreateInfo));
         }
@@ -49,6 +54,8 @@ namespace Scene
         INLINE void AddDirectionalLight(
             const Object::Light::LightCreateInfo& lightCreateInfo)
         {
+            IncrementObjNum();
+
             m_pLightList.push_back(
                 BX_NEW(Object::Light::DirectionalLight, *m_pObjArena)(lightCreateInfo));
         }
@@ -56,6 +63,8 @@ namespace Scene
         INLINE void AddPointLight(
             const Object::Light::PointLightCreateInfo& pointLightCreateInfo)
         {
+            IncrementObjNum();
+
             m_pLightList.push_back(
                 BX_NEW(Object::Light::PointLight, *m_pObjArena)(pointLightCreateInfo));
         }
@@ -63,6 +72,8 @@ namespace Scene
         INLINE void AddSpotLight(
             const Object::Light::SpotLightCreateInfo& spotLightCreateInfo)
         {
+            IncrementObjNum();
+
             m_pLightList.push_back(
                 BX_NEW(Object::Light::SpotLight, *m_pObjArena)(spotLightCreateInfo));
         }
@@ -76,7 +87,49 @@ namespace Scene
                 BX_NEW(Object::Model::ModelObject, *m_pObjArena)(modelFile, materialFile, modelTrans));
         }
 
+        INLINE Object::Camera::CameraBase* GetCamera(
+            const UINT camIndex) const
+        {
+            return m_pCameraList[camIndex];
+        }
+
+        INLINE Object::Light::LightBase* GetLight(
+            const UINT lightIndex) const
+        {
+            return m_pLightList[lightIndex];
+        }
+
+        INLINE Object::Model::ModelObject* GetModel(
+            const UINT modelIndex) const
+        {
+            return m_pModelList[modelIndex];
+        }
+
+        static INLINE size_t GetMaxObjSize()
+        {
+            size_t maxSize = 0;
+
+            maxSize = std::max(maxSize, sizeof(Object::Camera::ProspectiveCamera));
+            maxSize = std::max(maxSize, sizeof(Object::Camera::OrthographicCamera));
+            maxSize = std::max(maxSize, sizeof(Object::Light::DirectionalLight));
+            maxSize = std::max(maxSize, sizeof(Object::Light::PointLight));
+            maxSize = std::max(maxSize, sizeof(Object::Light::SpotLight));
+            maxSize = std::max(maxSize, sizeof(Object::Model::ModelObject));
+
+            return maxSize;
+        }
+
     private:
+        INLINE void IncrementObjNum()
+        {
+            m_objNum++;
+
+            assert(m_objNum <= m_maxObjNum);
+        }
+
+        // The all object type supported by scene
+        static const std::vector<const type_info*> m_typeList;
+
         const Setting* const           m_pSetting;
 
         Memory::MemoryPoolArena* const m_pObjArena;
@@ -84,5 +137,8 @@ namespace Scene
         std::vector<Object::Camera::CameraBase*> m_pCameraList;
         std::vector<Object::Light::LightBase*>   m_pLightList;
         std::vector<Object::Model::ModelObject*> m_pModelList;
+
+        UINT                                     m_objNum;
+        UINT                                     m_maxObjNum;
     };
 }
